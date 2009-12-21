ff50051156c34bbfb06610e91c2029f597ce2929
Joshua Hull <joshbuddy@gmail.com>
2009-12-21 17:01:33 -0500

    just dump this niceity.

--- lib/messed/tasks/runner.rb
+++ lib/messed/tasks/runner.rb
@@ -2,9 +2,9 @@ class Messed
   module Tasks
     class Runner < Thor
 
-      method_options %w( detach -d ) => false
       method_options %w( environment -e ) => "development"
-      desc "interface [NAME]", "starts an interface (#{Messed::Booter.possible_interfaces($root).join(', ')})"
+      method_options %w( detach -d ) => false
+      desc "interface [NAME]", "starts an interface"
       def interface(name)
         Messed::Booter.new($root, :detach => options.detach?, :environment => options.environment) do |booter|
           interface = booter.interface_for(name.to_sym)
@@ -13,10 +13,8 @@ class Messed
         end
       end
   
-      method_options %w( status_port -s ) => 9000
-      method_options %w( pid -p ) => nil
-      method_options %w( detach -d ) => false
       method_options %w( environment -e ) => "development"
+      method_options %w( detach -d ) => false
       desc "application [NAME]", "start the application"
       def application
         Messed::Booter.new($root, :detach => options.detach?, :environment => options.environment) do |booter|
