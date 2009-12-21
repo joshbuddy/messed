638f67efde14bbfcf40317514d76a083647ca960
Joshua Hull <joshbuddy@gmail.com>
2009-12-21 14:45:57 -0500

    removed dead options

--- lib/messed/tasks/runner.rb
+++ lib/messed/tasks/runner.rb
@@ -2,14 +2,12 @@ class Messed
   module Tasks
     class Runner < Thor
 
-      method_options %w( status_port -s ) => 9000
-      method_options %w( pid -p ) => nil
       method_options %w( detach -d ) => false
       method_options %w( environment -e ) => "development"
       desc "interface [NAME]", "starts an interface (#{Messed::Booter.possible_interfaces($root).join(', ')})"
       def interface(name)
-        Messed::Booter.new($root, :detach => options.detach?, :pid_file => options.pid, :environment => options.environment, :status_port => options.status_port) do |booter|
-          interface = booter.interface_for(name)
+        Messed::Booter.new($root, :detach => options.detach?, :environment => options.environment) do |booter|
+          interface = booter.interface_for(name.to_sym)
           raise("unable to find an interface with name the `#{name}'") unless interface
           interface.start
         end
@@ -21,7 +19,7 @@ class Messed
       method_options %w( environment -e ) => "development"
       desc "application [NAME]", "start the application"
       def application
-        Messed::Booter.new($root, :detach => options.detach?, :pid_file => options.pid, :environment => options.environment, :status_port => options.status_port) do |booter|
+        Messed::Booter.new($root, :detach => options.detach?, :environment => options.environment) do |booter|
           application = booter.application
           application.start
         end
