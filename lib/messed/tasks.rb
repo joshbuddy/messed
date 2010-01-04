ebbc0c32e93f9d77345ff1b6485d830b9f18b706
Joshua Hull <joshbuddy@gmail.com>
2010-01-03 23:49:03 -0500

    added status class

--- lib/messed/tasks.rb
+++ lib/messed/tasks.rb
@@ -2,6 +2,7 @@ class Messed
   module Tasks
     autoload :Generation, File.join('messed', 'tasks', 'generation')
     autoload :Runner,     File.join('messed', 'tasks', 'runner')
+    autoload :Status,     File.join('messed', 'tasks', 'status')
     autoload :Web,        File.join('messed', 'tasks', 'web')
     autoload :Console,    File.join('messed', 'tasks', 'console')
   end
